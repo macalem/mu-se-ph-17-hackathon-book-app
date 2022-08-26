baa473dcd0e3b17924cafc34bc73584ec7d6ed75
Karl <85920936+pastrana-karl@users.noreply.github.com>
2022-08-26 12:04:32 +0800

    Navigation and UI router
    This has the navigation bar for the home page and UI router

---
+++ react/src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from '../../components/navBar/NavBar';
+import LandingHeader from '../../components/LandingHeader/LandingHeader';
+
+function LandingPage() {
+  return (
+    <>
+      <Nav />
+      <LandingHeader />
+    </>
+  )
+}
+
+export default LandingPage
