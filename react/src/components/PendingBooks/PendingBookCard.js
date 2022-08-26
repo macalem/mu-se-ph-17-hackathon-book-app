3be3793afc3121d6df2a402b05e1dae0b7280d76
Paulo Jerome De Leon <deleopa@mliph.manulife.com.ph>
2022-08-26 18:01:17 +0800

    added admin page

---
+++ react/src/components/PendingBooks/PendingBookCard.js
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Typography from '@mui/material/Typography';
+import { Button, Grid, Box, ThemeProvider } from '@mui/material';
+import Theme from '../../const/theme';
+
+export default function RecipeReviewCard(props) {
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
+    return (
+        <ThemeProvider theme={Theme}>
+            <Card sx={{ maxWidth: 345 }}>
+                <CardHeader
+                    title={props.title}
+                />
+                <CardMedia
+                    component="img"
+                    height="194"
+                    image={props.image}
+                    alt="Noli Me Tangere"
+                />
+                <CardContent>
+                    <Typography variant="body2" color="text.secondary">by {props.author}</Typography>
+                    <CardActions disableSpacing>
+                        <Box sx={{ margin: 'auto' }}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={6}><Button variant="contained" color='primary'>Accept</Button></Grid>
+                                <Grid item xs={6}><Button variant="outlined" color='error'>Reject</Button></Grid>
+                            </Grid>
+                        </Box>
+                    </CardActions>
+                </CardContent>
+            </Card>
+        </ThemeProvider>
+    );
+}
--- react/src/pages/AdminPage/AdminPage.js
+++ react/src/pages/AdminPage/AdminPage.js
@@ -8,58 +8,28 @@ import Footer from '../../components/footer/Footer';
 import '../LandingPage/LandingPage.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import BookDetailsPage from './BookDetails';
-
-// Images
-import Book1 from '../../assets/image 5.png';
-import Book2 from '../../assets/image 7.png';
-import Book3 from '../../assets/image 8.png';
-
-const books = [
-    {
-      id: 1,
-      name: "Noli Me Tangere",
-      image: Book1,
-      author: "Pepe"
-    },
-    {
-      id: 2,
-      name: "Alamat ng Alitaptap",
-      image: Book2,
-      author: "Mang Jose"
-    },
-    {
-      id: 3,
-      name: "Alamat ng Durian",
-      image: Book3,
-      author: "Mang Pepe"
-    },
-    {
-      id: 4,
-      name: "Alamat ng Durian",
-      image: Book3,
-      author: "Mang Pepe"
-    },
-  ]
+import Books from '../../const/books';
+import PendingBookCards from '../../components/PendingBooks/PendingBookCard';
 
 function AdminPage() {
-    return (
-      <>
-        <Nav />
-        <Grid container spacing={12} id="grid-container">
+  return (
+    <>
+      <Nav />
+      <Grid container spacing={12} id="grid-container">
         {
-          books.map(book => {
+          Books.map(book => {
             return (
               <Grid xs={4}>
-                <BookCard title={book.name} author={book.author} image={book.image}/>
-          
+                <PendingBookCards title={book.name} author={book.author} image={book.image} />
+
               </Grid>
             )
           })
-      }
-        
+        }
+
       </Grid>
-      </>
-    )
-  }
-  
-  export default AdminPage
+    </>
+  )
+}
+
+export default AdminPage
