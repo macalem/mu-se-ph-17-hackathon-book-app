057129d7ab9acc4db99f57dbdd27d7fc15fe6183
doornab <doornab17@gmail.com>
2022-08-26 16:31:14 +0800

    initial commit admin page

--- react/src/App.js
+++ react/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './pages/LandingPage/LandingPage';
 import './App.css';
+import AdminPage from "./pages/AdminPage/AdminPage";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/admin" element={<AdminPage />} />
         </Routes>
       </Router>
     </div>
