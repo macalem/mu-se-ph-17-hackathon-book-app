b8cb0e8271cb726736e29f814d46acb5963c5aaf
MLIPH\Corpuan <corpuan@mfcgd.com>
2022-08-26 11:17:35 +0800

    landing page header, and landing page folder

---
+++ react/src/const/const.js
@@ -0,0 +1,20 @@
+import Header from "../assets/Header.png";
+
+const HeaderImages = [
+  {
+    labe: "1",
+    imgPath: Header,
+  },
+  {
+    label: "2",
+    imgPath:
+      "https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1890&q=80",
+  },
+  {
+    label: "3",
+    imgPath:
+      "https://images.unsplash.com/photo-1526243741027-444d633d7365?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80",
+  },
+];
+
+export default HeaderImages;
