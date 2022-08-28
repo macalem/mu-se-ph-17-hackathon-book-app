aaad98827d9c2937dec78c2077d4f91faa506a75
macalem <emmanuel_macalintal@manulife.ca>
2022-08-29 00:19:10 +0800

    Added reject and approve book functionalities

--- react/src/components/card/BookCard.js
+++ react/src/components/card/BookCard.js
@@ -3,24 +3,13 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { Button, ButtonGroup, Grid, Modal, ThemeProvider } from "@mui/material";
+import { ThemeProvider } from "@mui/material";
 import { useState } from "react";
 import "./BookCard.css";
 import Theme from "../../const/theme";
-import useAuth from "../../hooks/useAuth";
-import roles from "../../const/roles";
-// three PDF below are dummy data
-import Pdf from '../../assets/Alamat-ng-Alitaptap.pdf';
-import Pdf2 from '../../assets/Noli-me-Tangere.pdf';
-import Pdf3 from '../../assets/Alamat-ng-Durian.pdf';
+import BookModal from "../BookModal/BookModal";
 
 function BookCard(props) {
-
-  let regexPropsName = props.title;
-  regexPropsName = regexPropsName.replace(/\s+/g, '-');
-
-  const { auth } = useAuth();
-
   const [open, setOpen] = useState(false);
   const handleOpen = () => {
     setOpen(true);
@@ -29,39 +18,7 @@ function BookCard(props) {
     setOpen(false);
   };
 
-  const onButtonClick = () => {
-    // using Java Script method to get PDF file
-    // fetch(props.file).then((response) => {
-    fetch(regexPropsName === "Alamat-ng-Alitaptap" ? 
-    Pdf : regexPropsName === "Noli-me-Tangere" ? 
-    Pdf2 : regexPropsName === "Alamat-ng-Durian" ? 
-    Pdf3 : "")
-    .then((response) => {
-      response.blob().then((blob) => {
-        // Creating new object of PDF file
-        const fileURL = window.URL.createObjectURL(blob);
-        // Setting various property values
-        let alink = document.createElement("a");
-        alink.href = fileURL;
-        alink.download = `${props.title}.pdf`;
-        alink.click();
-      });
-    });
-  };
-
-  const style = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: 400,
-    bgcolor: "background.paper",
-    border: "2px solid #000",
-    boxShadow: 24,
-    pt: 2,
-    px: 4,
-    pb: 3,
-  };
+  const updateBookStatus = (id) => props.updateBookStatus(id)
 
   return (
     <>
@@ -71,20 +28,20 @@ function BookCard(props) {
           sx={{ display: "flex" }}
           className={props.premium ? "book-card-premium" : "book-card"}
         >
-          <Box sx={{ display: "flex", flexDirection: "column" }}>
+          <Box sx={{ display: "flex", flexDirection: "column", height: "300px", textAlign:"left" }}>
             <CardContent className="card-description" sx={{ flex: "1 0 auto" }}>
-              <Typography className="card-text" component="div" variant="h5">
+              <Typography className="card-text card-text-title" component="div" variant="h5">
                 {props.title}
               </Typography>
               <Typography
-                className="card-text"
+                className="card-text card-text-author"
                 variant="subtitle1"
                 color="text.secondary"
                 component="div"
               >
-                {props.author}
+                by {props.author}
               </Typography>
-            {props.premium && <Typography  className="card-description-premium" component="div" variant="">
+            {props.premium && <Typography className="card-description-premium" component="div" variant="">
                 PREMIUM
               </Typography> }
             </CardContent>
@@ -96,110 +53,22 @@ function BookCard(props) {
             alt={props.title}
           />
         </Card>
-        <Modal
+
+        <BookModal
           open={open}
-          onClose={handleClose}
-          aria-labelledby="parent-modal-title"
-          aria-describedby="parent-modal-description"
-        >
-          <Box sx={{ ...style, width: 700 }}>
-            <Grid container columnSpacing={3}>
-              <Grid item xs={6}>
-                <CardMedia
-                  component="img"
-                  sx={{ width: "500", height: "auto" }}
-                  image={props.image}
-                  alt="Noli Me Tangere"
-                />
-              </Grid>
-              <Grid item xs={6}>
-                <Grid container rowSpacing={1}>
-                  <Grid item xs={12}>
-                    <Typography variant="h4" fontWeight={500}>
-                      {" "}
-                      {props.title}{" "}
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={6}>
-                    <Typography variant="body1" fontWeight={500}>
-                      {" "}
-                      AUTHOR
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={6}>
-                    <Typography variant="body1"> {props.author} </Typography>
-                  </Grid>
-                  <Grid item xs={6}>
-                    <Typography variant="body1" fontWeight={500}>
-                      {" "}
-                      DEWEY DECIMAL{" "}
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={6}>
-                    <Typography variant="body1"> {props.dewey} </Typography>
-                  </Grid>
-                  <Grid item xs={6}>
-                    <Typography variant="body1" fontWeight={500}>
-                      {" "}
-                      ISBN{" "}
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={6}>
-                    <Typography variant="body1"> {props.isbn} </Typography>
-                  </Grid>
-                  <Grid item xs={6}>
-                    <Typography variant="body1" fontWeight={500}>
-                      {" "}
-                      FIRST PUBLISHED{" "}
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={6}>
-                    <Typography variant="body1">
-                      {" "}
-                      {props.published_date}{" "}
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Typography variant="body1">
-                      {" "}
-                      {props.description}{" "}
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={12} sx={{ margin: "auto" }}>
-                    <ButtonGroup>
-                      <Button variant="contained" color="warning">
-                        {/* Read */}
-                      <a href = {regexPropsName === "Alamat-ng-Alitaptap" ? 
-                      Pdf : regexPropsName === "Noli-me-Tangere" ? 
-                      Pdf2 : regexPropsName === "Alamat-ng-Durian" ? 
-                      Pdf3 : ""} target="_blank">Read</a>
-                      </Button>
-                      <Button
-                        variant="contained"
-                        color="secondary"
-                        onClick={onButtonClick}
-                      >
-                        Download
-                      </Button>
-                    </ButtonGroup>
-                    {props.status === "PENDING" && auth?.user && auth?.roles.includes(roles.Admin) ? (
-                      <ButtonGroup>
-                        <Button variant="contained" color="warning">
-                          Accept
-                        </Button>
-                        <Button variant="contained" color="error">
-                          Reject
-                        </Button>
-                      </ButtonGroup>
-                    ) : (
-                      ""
-                    )}
-                  </Grid>
-                </Grid>
-              </Grid>
-            </Grid>
-          </Box>
-        </Modal>
+          handleClose={handleClose}
+          updateBookStatus={updateBookStatus}
+          bookID={props.bookID}
+          title={props.title}
+          status={props.status}
+          image={props.image}
+          author={props.author}
+          dewey={props.dewey}
+          isbn={props.isbn}
+          published_date={props.published_date}
+          description={props.description}
+          file={props.file}
+        />
       </ThemeProvider>
     </>
   );
