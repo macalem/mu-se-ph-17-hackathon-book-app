3a744c5f9669cc16d59a4e2e331c4310d48b8160
Dabons22 <dabons22@gmail.com>
2022-08-29 10:44:41 +0800

    pageNotFoundImage

--- books_api/data/books.js
+++ books_api/data/books.js
@@ -20,7 +20,7 @@ export default [
         description: "this is book two",
         author: "Mang Jose",
         published_date: "2002-01-01",
-        genre_id: 1,
+        genre_id: 2,
         premium: 1,
         file: "http://fileshare/book2.pdf",
         isbn: "11111",
@@ -49,12 +49,14 @@ export default [
         author: "Mang Kepyeng",
         published_date: "2004-01-01",
         genre_id: 2,
-        premium: 1,
+        premium: 0,
         file: "http://fileshare/book3.pdf",
         isbn: "22222",
         cover: "/assets/image 8.png",
         status: "APPROVED"
-    },	{     
+    },
+    // NEW DATA 
+        	{     
             id:"6",
             name: "Ancient Beliefs and Customs of the Tagalogs",
             dewey_decimal: "7123",
@@ -62,11 +64,11 @@ export default [
             author: "M. Jean-Paul G. POTET",
             published_date: "2017-11-18" ,
             genre_id: 2,
-            premium: 1,
+            premium: 0,
             file: "http://fileshare/book1.pdf",
             isbn: "000",
-            cover:  "/assets/image 5.png",
-            status: "PENDING"
+            cover:  "/assets/acientbeliefs.jpg",
+            status: "APPROVED"
         },
         {
             id:"7",
@@ -75,10 +77,11 @@ export default [
             description: "Payatas, a 50-acre dump northeast of Manila’s Quezon City, is home to thousands of people who live off of what they can scavenge there. It is one of the poorest neighborhoods in a city whose law enforcement is already stretched thin, devoid of forensic resources and rife with corruption. So when the eviscerated bodies of preteen boys begin to appear in the dump heaps, there is no one to seek justice on their behalf.",
             author: "F.H. Batacan ",
             published_date: "2002-9-23", 
-            premium: "1",
+              genre_id: 2,
+              premium: 0,
             file: "http://fileshare/book1.pdf",
             isbn: "000",
-            cover:  "/assets/image 5.png",
+            cover:  "/assets/smc.jpg",
             status: "PENDING" 
         },
         {
@@ -88,11 +91,11 @@ export default [
             description: "The Bone Witch is a young adult fantasy novel written by Rin Chupeco. Published in February 2017, it is the first novel of Chupeco's second series. It was followed by two sequels: The Heart Forger in 2018 and The Shadowglass in 2019",
             author: "Rin Chupeco", 
             published_date: "2017-03-15", 
-            genre_id: "2",
-            premium: "1",
+            genre_id: 2,
+            premium: 0,
             file: "http://fileshare/book1.pdf",
             isbn: "000",
-            cover:  "/assets/image 5.png",
+            cover:  "/assets/theboneswitch.jpg",
             status: "APPROVED" 
         },{
         
@@ -102,11 +105,11 @@ export default [
             description: "It’s all fun and games until someone catches feelings If there’s something Pio Alvez is good at, it’s pretending to be someone he’s not. On stage and on-screen, the actor has mastered the art of becoming the characters he plays, and his new role should be no different. After all, how difficult would it be to pretend to be smitten with a beautiful, intelligent go-getter like Audrey Alonzo? Perhaps it’s more difficult pretending not to be.",
             author: "Tara Frejas" ,
             published_date: "2017-10-17",
-            genre_id: "2",
-            premium: "1",
+            genre_id: 2,
+            premium: 1,
             file: "http://fileshare/book1.pdf",
             isbn: "000",
-            cover:  "/assets/image 5.png",
+            cover:  "/assets/likeno.jpg",
             status: "APPROVED"
         },{
             id:"10",
@@ -115,26 +118,26 @@ export default [
             description: "In The Country is a stunning, lyrical work of fiction presented in the form of nine short stories. In 2016, Mia Alvar put contemporary Filipina authors on the map with her first published piece, giving voice to Filipino men and women in the diaspora. Her short stories about emigrants, wanderers, exiles, and expats across the globe expertly distinguishes the Philippine experience for each protagonist, while upholding the universal likeness of all Filipinos around the world and “in the country.” The tales of a migrant worker in Saudi Arabia, the sighting of a “White Lady,” and a New York pharmacist smuggling drugs to his sickly father in Manila will illicit nostalgia for many Filipinos looking for glimpses of home.", 
             author: "MIA ALVAR",
             published_date: "2015-6-16", 
-            genre_id: "3",
-            premium: "1",
+            genre_id: 3,
+            premium: 1,
             file: "http://fileshare/book1.pdf",
             isbn: "000",
-            cover:  "/assets/image 5.png",
+            cover:  "/assets/inthecountry.jpg",
             status: "APPROVED"
                 },
     
-        {
+            {
             id: "11",
             name:  "Po-on",
             dewey_decimal: "2051-231",
             description: "Po-on A Novel is a novel written by Francisco Sionil José, a Filipino English-language writer. This is the original title when it was first published in the Philippines in the English language. In the United States, it was published under the title Dusk: A Novel",  
             author: "F. Sionil José",
             published_date:  "1984-5-22",
-            genre_id: "2",
-            premium: "1",
+            genre_id: 2,
+            premium: 1,
             file: "http://fileshare/book1.pdf",
             isbn: "000",
-            cover:  "/assets/image 5.png",
+            cover:  "/assets/poon.jpg",
             status: "APPROVED"
         },
        {
@@ -144,11 +147,11 @@ export default [
             description: "A high-powered OB/GYN, Diana Gallagher-Cary, finds herself at a crossroads after a series of unfortunate events in her life. But when she uncovers a box of letters from her supposedly dead grandfather to her grandmother in their youth, she sojourns to the Philippines to reconnect with her long-lost family members. Along the way, she contends with her identity, her definition of family, while finding love in the unlikeliest of places.",
             author: "TIF MARCELO",
             published_date:  "2020-3-3",
-            genre_id: "1",
-            premium: "1",
+            genre_id: 1,
+            premium: 1,
             file: "http://fileshare/book1.pdf",
             isbn: "000",
-            cover:  "/assets/image 5.png",
+            cover:  "/assets/onceuponasunset.jpg",
             status: "APPROVED"
         },
         {
@@ -158,11 +161,11 @@ export default [
             description: "This novel written by Miguel Syjuco landed him a spot on the 2008 Man Asian Literary Prize, being awarded the Grand Prize. Layered with fiction and non-fiction themes, the story begins with main character Crispin Salvador, a noted writer whose lifeless body was discovered by authorities floating off the Hudson River in New York. Since there was no evidence of foul play, it led them to think that the author must have taken his own life. His student and friend, who has the same name as the book’s author Miguel Syjuco, hopes to unravel the story behind this mystery.",  
             author: "Miguel Syjuco",
             published_date:  "2008-12-12",
-            genre_id: "2",
+            genre_id: 2,
             premium: 1,
             file: "http://fileshare/book1.pdf",
             isbn: "000",
-            cover:  "/assets/image 5.png",
+            cover:  "/assets/illustrado.jpg",
             status: "PENDING"
         },
         {
@@ -172,39 +175,39 @@ export default [
             description: "The Marcos era in the ’70s is a dark chapter in Philippine history. No one dares to speak against the government for fear of execution – in some cases, bodies are not even retrieved. Hence, it has been a tumultuous period, especially for the Filipino parents to protect their kids from the oppressive regime and at the same time, stand together as one family. Lualhati Bautista has captured true-to-life scenarios in the ’70s, mentioning changes that arose after the Plaza Miranda bombing and the suspension of the Writ of Habeas Corpus in the Philippines. The novels tells the story of lead protagonist Amanda Bartolome and her family, where she had to deal with facing the law and her responsibilities towards her five sons.", 
             author: "Lualhati Bautista",
             published_date: "1983-4-23", 
-            genre_id: "1",
-            premium: "1",
+            genre_id: 2,
+            premium: 1,
             file: "http://fileshare/book1.pdf",
             isbn: "000",
-            cover:  "/assets/image 5.png",
+            cover:  "/assets/dekada70.jpg",
             status: "PENDING"
         },
         {	
             id:"16",
-            name:  "Friendship and magic",
+            name:  "Sugar and Spite ",
             dewey_decimal: "3122-3123",
             description:  "Jolina can't take Claudine's bullying any longer! The taunts and teasing are too much. Though Jolina knows she's still in training to use her grandfather's arbularyo magic, she sneaks into his potions lab to get her revenge. Jolina brews a batch of gayuma, a powerful love potion. Magic comes with a cost and bad intentions beget bad returns. Controlling another person's ability to love -- or hate -- will certainly have consequences. The magic demands payment, and it is about to come for Jolina in the form of a powerful storm.",
             author: "Gail D. Villanueva",
             published_date:  "2019-2-12",
-            genre_id: "2",
-            premium: "1",
+            genre_id: 1,
+            premium: 0,
             file: "http://fileshare/book1.pdf",
             isbn: "000",
-            cover:  "/assets/image 5.png",
+            cover:  "/assets/sugaras.jpg",
             status: "PENDING"
         },
         {
             id: "17",
-            name: "Light and feel-good", 
+            name: "Hello, Universe", 
             dewey_decimal: "5510-235-112",
             description: "Heartwarming and empowering, Hello, Universe is a funny and easily-relatable story about unexpected friendships told from four intertwining points of view—two boys and two girls.  The 2018 Newbery Medal winner Hello, Universe celebrates bravery, friendship, and finding your inner bayani (hero). It’s the kind of book you’ll want to read to slow down after a long and tiring day." , 
             author: "Erin Entrada Kelly",
             published_date: "2006-7-19",  
-            genre_id: "2", 
-            premium: "1",
+            genre_id: 2, 
+            premium:0,
             file: "http://fileshare/book1.pdf",
             isbn: "000",
-            cover:  "/assets/image 5.png",
+            cover:  "/assets/Hello_Universe.jpg",
             status: "PENDING"
         },
         {
@@ -214,11 +217,11 @@ export default [
             description: "How to Read Now explores the politics and ethics of reading, and insists that we are capable of something better: a more engaged relationship not just with our fiction and our art, but with our buried and entangled histories. Smart, funny, galvanizing, and sometimes profane, Castillo attacks the stale questions and less-than-critical proclamations that masquerade as vital discussion: reimagining the cartography of the classics, building a moral case against the settler-colonialism of lauded writers like Joan Didion, taking aim at Nobel Prize winners and toppling indie filmmakers, and celebrating glorious moments in everything from popular TV like The Watchmen to the films of Wong Kar-wai and the work of contemporary poets like Tommy Pico.",
             author:  "Elaine Castillo", 
             published_date: "2016-7-22", 
-            genre_id: "2",
-            premium: "1",
+            genre_id: 1,
+            premium: 0,
             file: "http://fileshare/book1.pdf",
             isbn: "000",
-            cover:  "/assets/image 5.png",
+            cover:  "/assets/How_To_Read_Now.jpg",
             status: "APPROVED"
            },
         {
@@ -228,11 +231,11 @@ export default [
             description: "Short but heartfelt, The Last Time I’ll Write About You is a collection of poems on falling in love, painful heartbreaks, and moving on—finding the words you could be longing to say (or even hear).  Popular fiction and poetry author Dawn Lanuza featured beautiful and relatable poems that will serve as the perfect companion for anyone who has loved, lost, and emerged anew.",
             author: "Dawn Lanuza",
             published_date: "2019-2-5", 
-            genre_id:"2",
-            premium: "1",
+            genre_id:2,
+            premium: 0,
             file: "http://fileshare/book1.pdf",
             isbn: "000",
-            cover:  "/assets/image 5.png",
+            cover:  "/assets/thelasttime.jpg",
             status: "APPROVED"
         },{
              id: "20",
@@ -241,11 +244,11 @@ export default [
             description: "Amboy: Recipes from the Filipino-American Dream is a biography by Alvin Cailan, arguably the most high-profile chef in America’s Filipino food movement. When he opened the now-legendary Eggslut in Los Angeles, a foodie cult favorite specializing in affordable but sophisticated egg sandwiches, he took the food scene by storm. This unique cookbook features scrumptious recipes and has a twist on Cailan’s personal experiences as an Amboy (the term for a Filipino raised in America) as he overcame cultural traditions and family expectations to find his path to success.", 
             author: "Alexandra Cuerdo and Alvin Cailan",
             published_date: "2018-5-23",  
-            genre_id: "2", 
-            premium: "1",
+            genre_id: 2, 
+            premium: 0,
             file: "http://fileshare/book1.pdf",
             isbn: "000",
-            cover:  "/assets/image 5.png",
+            cover:  "/assets/Amboy.jpeg",
             status: "APPROVED"
             }		
 
