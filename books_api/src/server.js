01a56636e76767334a2a2538b747a7231a1f9500
doornab <doornab17@gmail.com>
2022-08-29 20:37:12 +0800

    add swagger books api

--- books_api/src/server.js
+++ books_api/src/server.js
@@ -2,12 +2,17 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 const { PORT = 5000 } = process.env;
-
+import cors from "cors";
 import booksRouter from "./routes/books.js";
 import genresRouter from "./routes/genres.js";
+import swaggerUI from "swagger-ui-express";
+import fs from 'fs';
 
 var app = express();
-
+const swaggerJson = JSON.parse(
+  fs.readFileSync('src/swagger.json')
+);
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -15,7 +20,7 @@ app.use(cookieParser());
 
 app.use("/books/genres", genresRouter);
 app.use("/books", booksRouter);
-
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJson));
 app.get("*", function (req, res) {
   res.status(404).send("404 not found");
 });
